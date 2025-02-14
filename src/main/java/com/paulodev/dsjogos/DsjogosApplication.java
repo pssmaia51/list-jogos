e056b53edff181b0268954184a9254aab61910cd
Paulo Maia <santosmaia31@gmail.com>
2025-02-14 12:30:45 -0300

    Criando projeto

---
+++ src/main/java/com/paulodev/dsjogos/DsjogosApplication.java
@@ -0,0 +1,13 @@
+package com.paulodev.dsjogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsjogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsjogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulodev/dsjogos/DsjogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulodev.dsjogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsjogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
